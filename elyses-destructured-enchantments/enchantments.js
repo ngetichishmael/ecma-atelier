bd005977f512b1f2c306f5cca66c2ee158a7b380
ngetich ishmael <ngetichishmael786@gmail.com>
2026-09-11 21:35:48 +0300

    feat(lucky-numbers): implement twoSum, luckyNumber, and errorMessage functions

---
+++ elyses-destructured-enchantments/enchantments.js
@@ -0,0 +1,71 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Get the first card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the first card in the deck
+ */
+export function getFirstCard(deck) {
+  throw new Error('Implement the getFirstCard function');
+}
+
+/**
+ * Get the second card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the second card in the deck
+ */
+export function getSecondCard(deck) {
+  throw new Error('Implement the getSecondCard function');
+}
+
+/**
+ * Switch the position of the two cards
+ *
+ * @param {[Card, Card]} deck
+ *
+ * @returns {[Card, Card]} new deck with the 2 cards swapped
+ */
+export function swapTwoCards(deck) {
+  throw new Error('Implement the swapTopTwoCards function');
+}
+
+/**
+ * Rotate (shift) the position of the three cards (by one place)
+ *
+ * @param {[Card, Card, Card]} deck
+ *
+ * @returns {[Card, Card, Card]} new deck with the 3 cards shifted by one position
+ */
+export function shiftThreeCardsAround(deck) {
+  throw new Error('Implement the discardTopCard function');
+}
+
+/**
+ * Grab the chosen pile from the available piles
+ *
+ * @param {{ chosen: Card[], disregarded: Card[] }} piles
+ *
+ * @returns {Card[]} the pile named chosen
+ */
+export function pickNamedPile(piles) {
+  // 🚨 Do NOT use piles.chosen or piles.disregarded.
+  throw new Error('Implement the pickNamedPile function');
+}
+
+/**
+ * Swap the chosen pile for the disregarded pile and the disregarded pile for the chosen pile
+ *
+ * @param {{ chosen: Card[], disregarded: Card[] }} piles
+ * @returns {{ chosen: Card[], disregarded: Card[] }} new piles where the two piles are swapped
+ */
+export function swapNamedPile(piles) {
+  // 🪄 Don't break the magic.
+  // 🚨 Do NOT use piles.chosen or piles.disregarded.
+  // 🚨 Do NOT touch the next line or Elyse will accidentally reveal the trick.
+  return { chosen, disregarded };
+}
--- lucky-numbers/lucky-numbers.js
+++ lucky-numbers/lucky-numbers.js
@@ -8,7 +8,7 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Remove this line and implement the function');
+  return Number(array1.join('')) + Number(array2.join(''));
 }
 
 /**
@@ -18,7 +18,8 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Remove this line and implement the function');
+  const asString = String(value);
+  return asString === asString.split('').reverse().join('');
 }
 
 /**
@@ -29,5 +30,13 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Remove this line and implement the function');
+  if (!input) {
+    return 'Required field';
+  }
+
+  if (!Number(input)) {
+    return 'Must be a number besides 0';
+  }
+
+  return '';
 }
